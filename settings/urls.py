b60cf7baa09572dc48d09bcc406c4a89baf6f48a
Fares Emad <faresemadx@gmail.com>
2023-10-05 21:09:47 +0200

    feat: add namespace and name to URL Patterns

--- settings/urls.py
+++ settings/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", include("blog.urls", namespace="blog")),
 ]
