611faeba81870ed52b26886f3cf5fef03ecdb68f
Fares Emad <faresemadx@gmail.com>
2023-10-05 20:51:26 +0200

    feat:Adding models to the administration site and customize it

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from blog.models import Post
+
 # Register your models here.
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "auther", "publish", "status")
+    list_filter = ("status", "created_at", "publish", "auther")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("auther",)
+    date_hierarchy = "publish"
+    ordering = ("status", "publish")
