ba039632cf3275afaf8ef950c9ede50ab44a669a
Fares Emad <faresemadx@gmail.com>
2023-10-05 21:01:24 +0200

    feat: write view function to return published posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+from blog.models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+    # render it with HttpResponse without template
+    output = ", ".join([str(post) for post in posts])
+    return HttpResponse(output)
