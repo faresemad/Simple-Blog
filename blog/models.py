560416f28ae48dc5b7208488655b016b526397e0
Fares Emad <faresemadx@gmail.com>
2023-10-05 20:43:55 +0200

    feat: Creating and applying migrations

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
 # Create your models here.
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    auther = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
 
     class Meta:
         ordering = ["-publish"]
