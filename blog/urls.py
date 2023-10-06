1cc341f43e506f33b31e85bb59f0be38560e8d36
Fares Emad <faresemadx@gmail.com>
2023-10-06 16:21:07 +0200

    feat: Modifying the URL patterns

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from blog.views import post_detail, post_list
 app_name = "blog"
 urlpatterns = [
     path("", post_list, name="post_list"),
-    path("<int:id>/", post_detail, name="post_detail"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>", post_detail, name="post_detail"
+    ),
 ]
