b052f1d7a53de2935bd3e6469b381d7264ee60ec
Fares Emad <faresemadx@gmail.com>
2023-10-06 19:38:30 +0200

    feat: Adding comments to the administration site

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Comment, Post
 
 # Register your models here.
 
@@ -16,3 +16,10 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ("auther",)
     date_hierarchy = "publish"
     ordering = ("status", "publish")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "post", "created", "active"]
+    list_filter = ["active", "created", "updated"]
+    search_fields = ["name", "email", "body"]
