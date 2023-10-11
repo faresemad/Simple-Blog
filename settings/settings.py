a72410566a0ce5bb19c4caa6a500c708e7ee5d6c
Fares Emad <faresemadx@gmail.com>
2023-10-11 12:57:02 +0200

    feat: Adding a sitemap to the site

--- settings/settings.py
+++ settings/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "taggit",
     "blog.apps.BlogConfig",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
 ]
 
 MIDDLEWARE = [
